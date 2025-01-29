3e36097e5691c3dbef537d4596160a61fa4ef9e0
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2025-01-29 14:52:27 +0300

    dd

--- src/main/java/com/project/demo/controller/Master/MasterController.java
+++ src/main/java/com/project/demo/controller/Master/MasterController.java
@@ -18,6 +18,7 @@ public class MasterController extends HttpServlet {
     public static String url = "jdbc:mysql://localhost:3306/pokemon";
     String user = "replicator";
     String password = "root";
+    String pcName = "";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -30,25 +31,28 @@ public class MasterController extends HttpServlet {
             throw new RuntimeException(e);
         }
 
-        InstancePc myPc = new InstancePc("192.168.88.27", "root", "root");
+        InstancePc myPc = new InstancePc("192.168.7.103", "root", "root");
         InstancePc remotePc = new InstancePc("192.168.7.158", "root", "root");
 
-        MasterConf ms = new MasterConf(remotePc);
-        try (Connection con = DriverManager.getConnection(ms.jdbcUrl(), "replicator", "root")) {
+//        MasterConf ms = new MasterConf(remotePc.getIp(), myPc.getIp(), user, password);
+        MasterConf ms = new MasterConf(myPc.getIp(),    remotePc.getIp(), user, password);
+        System.out.println(ms.jdbcUrl());
+        try (Connection con = DriverManager.getConnection(ms.jdbcUrlMaster(), "replicator", "root")) {
 
             String query = "show master status";
             try (Statement statement = con.createStatement()) {
                 ResultSet rs = statement.executeQuery(query);
                 rs.next();
 
+                System.out.println(rs.getString("File"));
                 ms.setMasterLogFile(rs.getString("File"));
                 ms.setPosition(rs.getString("Position"));
 
                 String logQuery = ms.generateMasterQuery();
-                statement.executeUpdate("STOP ")
-                statement.executeUpdate(logQuery);
+//                statement.executeUpdate("STOP ")
+//                statement.executeUpdate(logQuery);
 
-//                out.print(ms.getMasterLogFile());
+                out.print(ms.generateMasterQuery());
             }
 
         } catch (SQLException e) {
--- src/main/java/com/project/demo/master/MasterConf.java
+++ src/main/java/com/project/demo/master/MasterConf.java
@@ -3,24 +3,30 @@ package com.project.demo.master;
 public class MasterConf {
     String masterLogFile;
     String position;
+    String ip;
     String masterIp;
     String user;
     String password;
 
-    public MasterConf(String masterIp, String user, String password) {
+    public MasterConf(String ip ,String masterIp, String user, String password) {
+        this.ip = ip;
         this.masterIp = masterIp;
         this.user = user;
         this.password = password;
     }
 
+//    public MasterConf(InstancePc pc) {
+//        this.masterIp = pc.ip;
+//        this.user = pc.user;
+//        this.password = pc.password;
+//    }
+
     public String jdbcUrl() {
-        return "jdbc:mysql://" + getMasterIp() + ":3306/pokemon";
+        return "jdbc:mysql://" + getIp() + ":3306/pokemon";
     }
 
-    public MasterConf(InstancePc pc) {
-        this.masterIp = pc.ip;
-        this.user = pc.user;
-        this.password = pc.password;
+    public String jdbcUrlMaster() {
+        return "jdbc:mysql://" + getMasterIp() + ":3306/pokemon";
     }
 
     public String getMasterLogFile() {
@@ -54,5 +60,29 @@ public class MasterConf {
                 "    MASTER_PASSWORD='" + password + "'," +
                 "    MASTER_LOG_FILE='" + masterLogFile + "'," +
                 "    MASTER_LOG_POS=" + position + ";";
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
